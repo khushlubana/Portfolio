3c21163ecddf9ba4e8832e920d32cbe39b7701c9
Khushwinder Singh <khushlubana47@gmail.com>
2021-12-04 00:13:04 -0500

    fixing projects

--- Links/Brampton/js/index.js
+++
@@ -1,7 +0,0 @@
-// let blurButton=document.querySelector("#blurButton");
-// let overlay=document.querySelector(".overlay");
-// let overlayIsOpen=document.querySelector(".overlayIsOpen");
-// blurButton.addEventListener('click',function(){
-//     overlayIsOpen.className="" ;
-//     overlay.style.display="none";
-// });
